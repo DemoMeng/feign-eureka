3e8266cfe53813469a78d872effb81a5398a9fd4
蒙启章 <mengqizhang@huluwa.cc>
2021-02-19 09:09:43 +0800

    服务上下线监听器

---
+++ src/main/java/com/mqz/feign/eureka/notify/NotifyListener.java
@@ -0,0 +1,44 @@
+package com.mqz.feign.eureka.notify;
+
+import com.netflix.appinfo.InstanceInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MarkerFactory;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mqz
+ * @description
+ * @abount https://github.com/DemoMeng
+ * @since 2021/2/5
+ */
+@Component
+public class NotifyListener {
+
+    private Logger logger = LoggerFactory.getLogger(NotifyListener.class);
+
+    /**
+     * 服务下线事件
+     * @param event
+     */
+    @EventListener
+    public void listenDown(EurekaInstanceCanceledEvent event){
+            // 发送邮件
+        logger.info(MarkerFactory.getMarker("DOWN")+"服务ID：" + event.getServerId() + "\t" +
+                "服务实例：" + event.getAppName() + "\t服务下线");
+        logger.info(event.getServerId() + "\t" + event.getAppName() + "服务下线");
+    }
+
+    /**
+     * 服务注册事件
+     * @param event
+     */
+    @EventListener
+    public void listenDown(EurekaInstanceRegisteredEvent event){
+        InstanceInfo instanceInfo = event.getInstanceInfo();
+        logger.info(MarkerFactory.getMarker("DOWN"),instanceInfo.getAppName() + "服务注册");
+    }
+}
